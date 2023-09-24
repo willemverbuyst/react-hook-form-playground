6f264070d7d25675c08357967ecc16887fc71e15
Willem Verbuyst <willemverbuyst@gmail.com>
2023-09-24 16:55:02 +0200

    Install dependencies, set up simple form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 function App() {
-  return <h1>Hello world!!</h1>;
+  return (
+    <main>
+      <h1>React Form Hook Playground</h1>
+      <form>
+        <section>
+          <label htmlFor="first-name">First Name</label>
+          <input id="first-name" type="text" />
+        </section>
+        <section>
+          <label htmlFor="last-name">First Name</label>
+          <input id="last-name" type="text" />
+        </section>
+        <button>Next</button>
+      </form>
+    </main>
+  );
 }
 
 export default App;
