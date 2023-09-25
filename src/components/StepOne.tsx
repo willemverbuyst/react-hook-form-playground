25df83c4c0e200160efd2c500a2e4ad6cfd6f9a8
Willem Verbuyst <willemverbuyst@gmail.com>
2023-09-25 08:18:24 +0200

    Add validation with zod for step one

--- src/components/StepOne.tsx
+++ src/components/StepOne.tsx
@@ -1,20 +1,28 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useStateMachine } from "little-state-machine";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { z } from "zod";
 import { updateStepOne } from "../actions";
 import Button from "./Button";
 
-type FormValues = {
-  firstName: string;
-  lastName: string;
-};
+const stepOneFormValues = z.object({
+  firstName: z.string().min(1, { message: "first name is required" }),
+  lastName: z.string().min(1, { message: "last name is required" }),
+});
+
+type StepOneFormvalues = z.infer<typeof stepOneFormValues>;
 
 function StepOne() {
   const navigate = useNavigate();
-  const { handleSubmit, register } = useForm<FormValues>();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<StepOneFormvalues>({ resolver: zodResolver(stepOneFormValues) });
   const { actions } = useStateMachine({ updateStepOne });
 
-  const onSubmit = (data: FormValues) => {
+  const onSubmit = (data: StepOneFormvalues) => {
     console.log(data);
     actions.updateStepOne(data);
     navigate("/step2");
@@ -31,22 +39,32 @@ function StepOne() {
           First Name
         </label>
         <input
-          {...register("firstName")}
           id="first-name"
+          {...register("firstName")}
           type="text"
           className="bg-slate-100 rounded px-2 py-1 outline-none text-slate-800"
         />
+        {errors.firstName && (
+          <p role="alert" className="text-red-400 py-1">
+            {errors.firstName.message}
+          </p>
+        )}
       </section>
       <section className="flex flex-col py-4">
         <label htmlFor="last-name" className="py-1">
-          First Name
+          Last Name
         </label>
         <input
-          {...register("lastName")}
           id="last-name"
+          {...register("lastName")}
           type="text"
           className="bg-slate-100 rounded px-2 py-1 outline-none text-slate-800"
         />
+        {errors.lastName && (
+          <p role="alert" className="text-red-400 py-1">
+            {errors.lastName.message}
+          </p>
+        )}
       </section>
       <section className="py-4 flex justify-end gap-2">
         <Button caption="Back" handleClick={handleGoBack} />
