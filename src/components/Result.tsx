68d259109709cc68cb2389b8bf63391a9a508782
Willem Verbuyst <willemverbuyst@gmail.com>
2023-09-25 06:20:19 +0200

    Work on display in Result component

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,11 +1,26 @@
 import { useStateMachine } from "little-state-machine";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 
 function Result() {
+  const navigate = useNavigate();
   const { state } = useStateMachine();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <section>
-      <h2>Result</h2>
-      {JSON.stringify(state)}
+    <section className="w-48 flex flex-col py-4">
+      <h2 className="text-3xl py-3">Result</h2>
+      <section className="py-3">
+        {Object.entries(state.data).map(([key, value], i) => (
+          <p key={i}>
+            {key}: {value}
+          </p>
+        ))}
+      </section>
+      <Button caption="back" handleClick={handleGoBack} />
     </section>
   );
 }
