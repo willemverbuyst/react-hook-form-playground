08539b302bca0df09d8ec9d878e4058ebb925bed
Willem Verbuyst <willemverbuyst@gmail.com>
2023-09-29 06:23:09 +0200

    Display result

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -19,12 +19,61 @@ function Result() {
   return (
     <section>
       <h2 className="text-4xl text-centers">Result</h2>
-      <section className="py-3">
-        {Object.entries(state.data || {}).map(([key, value], i) => (
-          <p key={i}>
-            {key}: {JSON.stringify(value)}
-          </p>
-        ))}
+      <section className="flex flex-col gap-5 py-3">
+        <section className="flex justify-between gap-8">
+          <p>First Name:</p>
+          <p>{state.data.firstName}</p>
+        </section>
+        <section className="flex justify-between gap-8">
+          <p>Last Name:</p>
+          <p>{state.data.lastName}</p>
+        </section>
+        <section className="flex justify-between gap-8">
+          <p>BSN:</p>
+          <p>{state.data.bsn}</p>
+        </section>
+        <section className="flex justify-between gap-8">
+          <p>Date of Birth:</p>
+          <p>{state.data.dateOfBirth?.toDateString()}</p>
+        </section>
+        <section className="flex justify-between gap-8">
+          <p>Email:</p>
+          <p>{state.data.email}</p>
+        </section>
+        <section className="flex justify-between gap-8">
+          <p>Phone Number:</p>
+          <p>{state.data.phoneNumber}</p>
+        </section>
+        <section className="flex justify-between gap-8">
+          <p>Files:</p>
+          <ul>
+            {Object.values(state.data.files || {}).map((f, i) => (
+              <li key={i} className="text-end">
+                {f.name}
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section className="flex justify-between gap-8">
+          <p>Position:</p>
+          <p>{state.data.position}</p>
+        </section>
+        <section className="flex justify-between gap-8">
+          <p>Skills:</p>
+          <ul>
+            {Object.values(state.data.skills)
+              .filter((i) => i.value)
+              .map((skill, i) => (
+                <li key={i} className="text-end">
+                  {skill.value}
+                </li>
+              ))}
+          </ul>
+        </section>
+        <section className="flex justify-between gap-8">
+          <p>Phone Number:</p>
+          <p>{state.data.phoneNumber}</p>
+        </section>
       </section>
       <section className="py-4 flex justify-end gap-2">
         <Button caption="back" handleClick={handleGoBack} />
